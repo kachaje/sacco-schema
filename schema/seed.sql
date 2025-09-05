145211af0cadc011ae530d05d9ead49a697902d0
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-05 20:16:10 +0200

    Added separate seed and trigger initation files

---
+++ schema/seed.sql
@@ -0,0 +1,80 @@
+INSERT
+OR IGNORE INTO userRole (name)
+VALUES
+  ("Default"),
+  ("Member"),
+  ("Admin"),
+  ("Cashier"),
+  ("Accountant"),
+  ("Loans Officer"),
+  ("Manager");
+
+INSERT
+OR IGNORE INTO user (username, password, name, role)
+VALUES
+  (
+    "default",
+    "$2a$10$Xo4x3KiCkB3xGKvaCI4Hn.Be95DEiaIT3lbvHx/kOmyx7IqGY6ILK",
+    "Default User",
+    "Default"
+  );
+
+WITH RECURSIVE
+  cnt (x) AS (
+    SELECT
+      1
+    UNION ALL
+    SELECT
+      x + 1
+    FROM
+      cnt
+    LIMIT
+      999999
+  ) INSERT
+  OR IGNORE INTO memberIdsCache (idNumber)
+SELECT
+  CONCAT ('KSM', SUBSTR ('000000' || x, -6)) AS id
+FROM
+  cnt
+ORDER BY
+  RANDOM ();
+
+WITH RECURSIVE
+  cnt (x) AS (
+    SELECT
+      1
+    UNION ALL
+    SELECT
+      x + 1
+    FROM
+      cnt
+    LIMIT
+      999999
+  ) INSERT
+  OR IGNORE INTO memberSharesIdsCache (idNumber)
+SELECT
+  CONCAT ('KSH', SUBSTR ('000000' || x, -6)) AS id
+FROM
+  cnt
+ORDER BY
+  RANDOM ();
+
+WITH RECURSIVE
+  cnt (x) AS (
+    SELECT
+      1
+    UNION ALL
+    SELECT
+      x + 1
+    FROM
+      cnt
+    LIMIT
+      999999
+  ) INSERT
+  OR IGNORE INTO memberSavingsIdsCache (idNumber)
+SELECT
+  CONCAT ('KSS', SUBSTR ('000000' || x, -6)) AS id
+FROM
+  cnt
+ORDER BY
+  RANDOM ();
---
+++ schema/triggers.sql
@@ -0,0 +1,71 @@
+---- START addMemberIdNumber TRIGGER ----
+CREATE TRIGGER IF NOT EXISTS addMemberIdNumber AFTER INSERT ON member FOR EACH ROW BEGIN
+UPDATE memberIdsCache
+SET
+  claimed = 1,
+  memberId = NEW.id
+WHERE
+  id = (
+    SELECT
+      id
+    FROM
+      memberIdsCache
+    WHERE
+      claimed = 0
+    ORDER BY
+      id
+    LIMIT
+      1
+  );
+
+UPDATE member
+SET
+  memberIdNumber = (
+    SELECT
+      idNumber
+    FROM
+      memberIdsCache
+    WHERE
+      memberId = NEW.id
+  )
+WHERE
+  id = NEW.id;
+
+---- END addMemberIdNumber TRIGGER ----
+END;
+
+---- START addMemberIdNumber TRIGGER ----
+CREATE TRIGGER IF NOT EXISTS addMemberIdNumber AFTER INSERT ON member FOR EACH ROW BEGIN
+UPDATE memberSavingIdsCache
+SET
+  claimed = 1,
+  memberSavingId = NEW.id
+WHERE
+  id = (
+    SELECT
+      id
+    FROM
+      memberSavingIdsCache
+    WHERE
+      claimed = 0
+    ORDER BY
+      id
+    LIMIT
+      1
+  );
+
+UPDATE member
+SET
+  memberSavingIdNumber = (
+    SELECT
+      idNumber
+    FROM
+      memberSavingIdsCache
+    WHERE
+      memberSavingId = NEW.id
+  )
+WHERE
+  id = NEW.id;
+
+---- END addMemberIdNumber TRIGGER ----
+END;
