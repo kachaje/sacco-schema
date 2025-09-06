584bd8a385cbc41f21d8f8901f44c7eaf1a3336f
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-06 07:32:48 +0200

    Added test fixtures for model graphs

--- modelGraph/modelGraph_test.go
+++ modelGraph/modelGraph_test.go
@@ -2,7 +2,6 @@ package modelgraph_test
 
 import (
 	"encoding/json"
-	"fmt"
 	"os"
 	"path/filepath"
 	modelgraph "sacco/modelGraph"
@@ -11,6 +10,7 @@ import (
 
 func TestCreateGraph(t *testing.T) {
 	data := map[string]any{}
+	target := map[string]any{}
 
 	content, err := os.ReadFile(filepath.Join("..", "schema", "models", "modelsData.json"))
 	if err != nil {
@@ -27,10 +27,23 @@ func TestCreateGraph(t *testing.T) {
 		t.Fatal(err)
 	}
 
-	payload, err := json.MarshalIndent(result, "", "  ")
+	if false {
+		payload, err := json.MarshalIndent(result, "", "  ")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		os.WriteFile(filepath.Join(".", "fixtures", "graph.json"), payload, 0644)
+	}
+
+	content, err = os.ReadFile(filepath.Join(".", "fixtures", "graph.json"))
 	if err != nil {
 		t.Fatal(err)
 	}
 
-	fmt.Println(string(payload))
+	err = json.Unmarshal(content, &target)
+	if err != nil {
+		t.Fatal(err)
+	}
+
 }
