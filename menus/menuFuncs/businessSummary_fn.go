e42b206430567889553e235f73b31424981ae82b
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-10 23:08:13 +0200

    Added business summary logic

--- menus/menuFuncs/businessSummary_fn.go
+++ menus/menuFuncs/businessSummary_fn.go
@@ -1,8 +1,27 @@
 package menufuncs
 
 import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"regexp"
 	"sacco/parser"
+	"strings"
+
+	_ "embed"
 )
+
+//go:embed templates/businessSummary.template.json
+var businessSummaryTemplateContent []byte
+
+var businessSummaryTemplateData map[string]any
+
+func init() {
+	err := json.Unmarshal(businessSummaryTemplateContent, &businessSummaryTemplateData)
+	if err != nil {
+		log.Fatalf("menus.init: %s", err.Error())
+	}
+}
 
 func BusinessSummary(
 	loadMenu func(
@@ -12,10 +31,51 @@ func BusinessSummary(
 	data map[string]any,
 	session *parser.Session,
 ) string {
-	var result string = "Business Summary\n\n" +
-		"00. Main Menu\n"
+	var phoneNumber, text, preferencesFolder string
+	var response string
 
-	_ = data
+	if data["phoneNumber"] != nil {
+		if val, ok := data["phoneNumber"].(string); ok {
+			phoneNumber = val
+		}
+	}
+	if data["text"] != nil {
+		if val, ok := data["text"].(string); ok {
+			text = val
+		}
+	}
+	if data["preferencesFolder"] != nil {
+		if val, ok := data["preferencesFolder"].(string); ok {
+			preferencesFolder = val
+		}
+	}
 
-	return result
+	if session != nil {
+		if strings.TrimSpace(text) == "99" {
+			parentMenu := "main"
+
+			if regexp.MustCompile(`\.\d+$`).MatchString(session.CurrentMenu) {
+				parentMenu = regexp.MustCompile(`\.\d+$`).ReplaceAllLiteralString(session.CurrentMenu, "")
+			}
+
+			session.CurrentMenu = parentMenu
+			text = ""
+			return loadMenu(session.CurrentMenu, session, phoneNumber, text, preferencesFolder)
+		} else {
+			data = LoadTemplateData(session.ActiveData, businessSummaryTemplateData)
+
+			table := TabulateData(data)
+
+			tableString := strings.Join(table, "\n")
+
+			response = "CON Business Summary\n" +
+				"\n" +
+				fmt.Sprintf("%s\n", tableString) +
+				"\n" +
+				"99. Cancel\n" +
+				"00. Main Menu"
+		}
+	}
+
+	return response
 }
