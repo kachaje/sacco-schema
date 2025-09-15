2be956e22227fb9d7ea5b4136f81201327adc661
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-15 13:57:06 +0200

    Added unit test for nested child cache queries

--- menus/cacheResolver_test.go
+++ menus/cacheResolver_test.go
@@ -181,3 +181,45 @@ func TestResolveCacheDataNestedL2(t *testing.T) {
 		t.Fatalf("Test failed; Expected: %#v; Actual: %#v", target, result)
 	}
 }
+
+func TestNestedChild(t *testing.T) {
+	data := map[string]any{}
+	cacheData := map[string]any{}
+	target := map[string]any{}
+
+	content, err := os.ReadFile(filepath.Join(".", "fixtures", "nestedChildSourceData.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, &data)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	content, err = os.ReadFile(filepath.Join(".", "fixtures", "nestedChildQueries.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, &cacheData)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	content, err = os.ReadFile(filepath.Join(".", "fixtures", "nestedChildTargetData.json"))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	err = json.Unmarshal(content, &target)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	result := menus.ResolveCacheData(data, "member.memberLoan.0.memberLoanLiability.0.")
+
+	if !utils.MapsEqual(target, result) {
+		t.Fatalf("Test failed; Expected: %#v; Actual: %#v", target, result)
+	}
+}
