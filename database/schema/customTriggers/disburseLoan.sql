b711d8afc7dcffb2087b191a944b54c83ee197ae
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-17 07:57:14 +0200

    Added trigger for creating loan disbursement once approved

---
+++ database/schema/customTriggers/disburseLoan.sql
@@ -0,0 +1,14 @@
+CREATE TRIGGER IF NOT EXISTS disburseLoan AFTER INSERT ON memberLoanVerification WHEN NEW.verified="Yes" BEGIN
+INSERT INTO
+    memberLoanDisbursement (memberLoanId, description, amount)
+SELECT
+    a.memberLoanId,
+    CONCAT ("Disbursement for loan number ", v.loanNumber) description,
+    a.amountRecommended AS amount
+FROM
+    memberLoanVerification v
+    LEFT JOIN memberLoanApproval a ON a.id=v.memberLoanApprovalId
+WHERE
+    v.id=NEW.id;
+
+END;
