78caaed0164a227d78e185ed790fd0e86508da55
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-17 15:55:35 +0200

    Added trigger logic for creating loan schedules when loans are approved

--- database/schema/customTriggers/disburseLoan.sql
+++ database/schema/customTriggers/disburseLoan.sql
@@ -38,4 +38,51 @@ SELECT
 FROM
   memberLoanDisbursement;
 
+WITH RECURSIVE
+  cnt (x) AS (
+    SELECT
+      1
+    UNION ALL
+    SELECT
+      x + 1
+    FROM
+      cnt
+    LIMIT
+      (
+        SELECT
+          repaymentPeriodInMonths
+        FROM
+          memberLoan
+        WHERE
+          id = 1
+      )
+  )
+SELECT
+  DATE (
+    current_timestamp,
+    CONCAT ('+', x, ' month'),
+    'start of month'
+  ) AS dueDate,
+  (i.amountRecommended - ((x -1) * i.instalment)) AS principal,
+  (i.amountRecommended - ((x -1) * i.instalment)) * i.monthlyInterestRate AS interest,
+  (i.amountRecommended - ((x -1) * i.instalment)) * i.monthlyInsuranceRate AS insurance,
+  CASE
+    WHEN x = 1 THEN (i.amountRecommended - ((x -1) * i.instalment)) * i.processingFeeRate
+    ELSE 0
+  END AS processingFee
+FROM
+  cnt,
+  (
+    SELECT
+      a.amountRecommended / l.repaymentPeriodInMonths AS instalment,
+      l.repaymentPeriodInMonths,
+      a.amountRecommended,
+      l.monthlyInterestRate,
+      l.monthlyInsuranceRate,
+      l.processingFeeRate
+    FROM
+      memberLoan l
+      LEFT JOIN memberLoanApproval a ON a.memberLoanId = l.id
+  ) AS i;
+
 END;
