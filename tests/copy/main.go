35d79368ef3abeaa5e091406dc8f44143935d251
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-18 08:58:46 +0200

    Added logic for cleaning up fixtures folder if empty

--- tests/copy/main.go
+++ tests/copy/main.go
@@ -43,14 +43,20 @@ func main() {
 			return err
 		}
 
+		folderName := filepath.Dir(pathName)
+		filename := filepath.Base(pathName)
+
 		if dir.IsDir() {
 			if regexp.MustCompile(`\.git*|\.vscode|tests|\.[A-Za-z]+`).MatchString(pathName) {
 				return filepath.SkipDir
+			} else if regexp.MustCompile(`fixtures`).MatchString(pathName) {
+				files, err := os.ReadDir(pathName)
+				if err == nil && len(files) == 0 {
+					os.RemoveAll(pathName)
+					return filepath.SkipDir
+				}
 			}
 		}
-
-		filename := filepath.Base(pathName)
-		folderName := filepath.Dir(pathName)
 
 		if strings.HasSuffix(filename, "_test.go") {
 			return copyFile(pathName, filepath.Join(rootFolder, "tests", filename))
