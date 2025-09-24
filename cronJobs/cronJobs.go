4593c9c069fa149e28b34e850bb04764aece5805
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-24 07:43:08 +0200

    More updates for cron jobs runner

--- cronJobs/cronJobs.go
+++ cronJobs/cronJobs.go
@@ -1,9 +1,38 @@
 package cronjobs
 
+import (
+	"log"
+	"sacco/database"
+)
+
 type CronJobs struct {
-	SavingsInterests func(targetDate string) error
+	Jobs map[string]func(targetDate string) error
+	DB   *database.Database
 }
 
-func Main() {
+func NewCronJobs(db *database.Database) *CronJobs {
+	jobs := &CronJobs{
+		DB:   db,
+		Jobs: map[string]func(targetDate string) error{},
+	}
 
+	jobs.Jobs["savingsInterests"] = jobs.CalculateSavingsInterests
+
+	return jobs
+}
+
+func (c *CronJobs) RunCronJobs(targetDate string) error {
+	for job, jobFn := range c.Jobs {
+		log.Printf("Running job %s\n", job)
+
+		if err := jobFn(targetDate); err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func (c *CronJobs) CalculateSavingsInterests(targetDate string) error {
+	return nil
 }
