bc8eb8e43549e6dd73b8f96aefa9f55f33520a90
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-09-25 10:54:54 +0200

    Added logic for calculation ordinary deposits interest

--- cronJobs/cronJobs.go
+++ cronJobs/cronJobs.go
@@ -1,6 +1,7 @@
 package cronjobs
 
 import (
+	"fmt"
 	"log"
 	"sacco/database"
 )
@@ -16,7 +17,7 @@ func NewCronJobs(db *database.Database) *CronJobs {
 		Jobs: map[string]func(targetDate string) error{},
 	}
 
-	jobs.Jobs["savingsInterests"] = jobs.CalculateSavingsInterests
+	jobs.Jobs["ordinaryDepositsInterest"] = jobs.CalculateOrdinaryDepositsInterest
 
 	return jobs
 }
@@ -33,10 +34,34 @@ func (c *CronJobs) RunCronJobs(targetDate string) error {
 	return nil
 }
 
-func (c *CronJobs) CalculateSavingsInterests(targetDate string) error {
-	_, err := c.DB.SQLQuery(`
-	
-	`)
+func (c *CronJobs) CalculateOrdinaryDepositsInterest(targetDate string) error {
+	_, err := c.DB.SQLQuery(fmt.Sprintf(`
+INSERT INTO memberSavingInterest (memberSavingId, description, amount, dueDate)
+WITH savings AS ( SELECT 
+	memberSavingId, 
+	CONCAT(STRFTIME('%%Y', transactionDate), ' - ',
+	CASE
+        WHEN CAST(STRFTIME('%%m', transactionDate) AS INTEGER) BETWEEN 1 AND 3 THEN 'Q1'
+        WHEN CAST(STRFTIME('%%m', transactionDate) AS INTEGER) BETWEEN 4 AND 6 THEN 'Q2'
+        WHEN CAST(STRFTIME('%%m', transactionDate) AS INTEGER) BETWEEN 7 AND 9 THEN 'Q3'
+        ELSE 'Q4'
+    END) AS description,
+	SUM(balance)/COUNT(id) AS average,
+	(SUM(balance)/COUNT(id)) * 0.1 * 0.25 AS interest
+FROM memberSavingTransaction 
+WHERE savingsTypeName = 'Ordinary Deposit'
+GROUP BY memberSavingId, description
+) 
+SELECT memberSavingId, description, interest, CURRENT_TIMESTAMP 
+FROM savings 
+WHERE description = CONCAT(STRFTIME('%%Y', '%s'), ' - ',
+	CASE
+        WHEN CAST(STRFTIME('%%m', '%s') AS INTEGER) BETWEEN 1 AND 3 THEN 'Q1'
+        WHEN CAST(STRFTIME('%%m', '%s') AS INTEGER) BETWEEN 4 AND 6 THEN 'Q2'
+        WHEN CAST(STRFTIME('%%m', '%s') AS INTEGER) BETWEEN 7 AND 9 THEN 'Q3'
+        ELSE 'Q4'
+  END)
+	`, targetDate, targetDate, targetDate, targetDate))
 	if err != nil {
 		return err
 	}
