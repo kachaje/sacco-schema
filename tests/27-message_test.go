cdbddc58f285a59fa40f7f9b0d32bb1f15c8caa9
Chimwemwe Kachaje <chimwemwe.kachaje@gmail.com>
2025-10-14 11:33:15 +0200

    Added unit test that can only conditionally run locally

---
+++ tests/27-message_test.go
@@ -0,0 +1,67 @@
+package tests
+
+import (
+	"fmt"
+	"os"
+	"testing"
+	"time"
+
+	"github.com/kachaje/firestore-webrtc/peer"
+	"github.com/kachaje/firestore-webrtc/utils"
+)
+
+func TestMessageToJson(t *testing.T) {
+	if os.Getenv("CI") != "" {
+		t.Skip()
+	}
+
+	timestamp := time.Now().UnixMilli()
+
+	msg := peer.NewMessage("Hello World", peer.TEXT_MESSAGE, "text/plain", "sample", &timestamp)
+
+	result, err := msg.MessageToJson()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	target := fmt.Sprintf(`
+{
+  "msgType": "textMessage",
+	"data": "Hello World",
+	"mimeType": "text/plain",
+	"sourceTimestamp": 0,
+	"sinkTimestamp": 0,
+  "sender": "sample",
+  "timestamp": %v
+}`, timestamp)
+
+	if utils.CleanString(target) != utils.CleanString(result) {
+		t.Fatal("Test failed")
+	}
+}
+
+func TestMessageToByte(t *testing.T) {
+	timestamp := time.Now().UnixMilli()
+
+	msg := peer.NewMessage("Hello World", peer.TEXT_MESSAGE, "text/plain", "sample", &timestamp)
+
+	result, err := msg.MessageToByte()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	target := fmt.Appendf(nil, `
+{
+  "msgType": "textMessage",
+	"data": "Hello World",
+	"mimeType": "text/plain",
+	"sourceTimestamp": 0,
+	"sinkTimestamp": 0,
+  "sender": "sample",
+  "timestamp": %v
+}`, timestamp)
+
+	if utils.CleanString(string(target)) != utils.CleanString(string(result)) {
+		t.Fatal("Test failed")
+	}
+}
